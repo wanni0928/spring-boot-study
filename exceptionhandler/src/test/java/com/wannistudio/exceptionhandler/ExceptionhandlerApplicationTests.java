fcc1d5f8bedda4c345c580ab3de63daa3fe97be8
wanni0928 <chlgudwnd123@gmail.com>
2020-12-13 23:00:39 +0900

    .

---
+++ exceptionhandler/src/test/java/com/wannistudio/exceptionhandler/ExceptionhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wannistudio.exceptionhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionhandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- thymeleaf/src/test/java/com/wannistudio/thymeleaf/ControllerTest.java
+++ thymeleaf/src/test/java/com/wannistudio/thymeleaf/ControllerTest.java
@@ -1,11 +1,16 @@
 package com.wannistudio.thymeleaf;
 
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,8 +23,11 @@ public class ControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    WebClient webClient;
+
     @Test
-    public void hello() throws Exception {
+    void hello() throws Exception {
         // req : /
         // res :
         // model name : wanni
@@ -30,5 +38,12 @@ public class ControllerTest {
                 .andExpect(view().name("hello"))
                 .andExpect(model().attribute("name", is("wanni")))
                 .andExpect(content().string(containsString("wanni")));
+    }
+
+    @Test
+    void testHtmlUnit() throws Exception {
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualTo("wanni");
     }
 }
