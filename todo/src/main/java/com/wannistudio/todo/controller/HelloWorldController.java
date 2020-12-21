1e7cf45948250951eb2c193a9cb9ad1a85cc7dac
wanni0928 <chlgudwnd123@gmail.com>
2020-12-21 22:37:28 +0900

    .

---
+++ todo/src/main/java/com/wannistudio/todo/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.wannistudio.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello";
+    }
+}
---
+++ todo/src/test/java/com/wannistudio/todo/controller/HelloWorldControllerTest.java
@@ -0,0 +1,28 @@
+package com.wannistudio.todo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void sayHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello"))
+        ;
+    }
+}
